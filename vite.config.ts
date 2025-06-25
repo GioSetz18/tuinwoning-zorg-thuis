b02a690620c27363ffa519bcfaae3e415dd28997
Siverty <justinschots13@gmail.com>
2025-06-25 12:09:10 +0200

    Github pages fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,12 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  // *** ABSOLUTE MUST for GitHub Pages ***
+  // If you ever rename the repo, update this string too.
+  base: "/tuinwoning-zorg-thuis/",
+  build: {
+    outDir: path.resolve(__dirname, "../dist"),
+  },
   server: {
     host: "::",
     port: 8080,
